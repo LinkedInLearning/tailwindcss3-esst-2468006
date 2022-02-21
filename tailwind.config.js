98f12ee5b09c5ffbb782410816a1a3c6db26b31f
Ray Villalobos <ray@planetoftheweb.com>
2022-02-21 17:03:37 -0500

    basic installation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: [],
+  content: ['./src/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+export default {
+  root: 'src',
+  publicDir: 'public',
+  outDir: '../docs',
+  server: {
+    open: true
+  }
+}
